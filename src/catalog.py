935dbcd377530cdca7e1480df519c70edea0dcd9
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-06-29 22:21:45 +0500

    init Product and Category

--- src/catalog.py
+++ src/catalog.py
@@ -1,13 +1,32 @@
 class Product:
-    """Класс для представления товара"""
+    """Класс для представления товара."""
     name: str
     description: str
     price: float
     quantity: int
 
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса."""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
 
 class Category:
-    """Класс для представления категории товаров"""
+    """Класс для представления категории товаров."""
+    categories_count = 0
+    products_count = 0
+
     name: str
     description: str
     products: list[Product]
+
+    def __init__(self, name, description, products):
+        """Метод для инициализации экземпляра класса."""
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.categories_count += 1
+        Category.products_count += len(products)
