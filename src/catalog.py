60a2695bfd845514f0cde8b9abc758160f4b2851
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-07-04 01:33:59 +0500

    add getter products

--- src/catalog.py
+++ src/catalog.py
@@ -38,4 +38,21 @@ class Category:
         self.__products.append(product)
         Category.products_count += 1
 
+    @property
+    def products(self) -> str:
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
 
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category("Смартфоны",
+                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         [product1, product2, product3])
+
+    print(category1.products)
