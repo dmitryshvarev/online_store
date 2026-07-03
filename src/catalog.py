ab25edbb8a7d8cd23f63a05117782e438a44fd42
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-07-04 03:53:27 +0500

    add price.getter and price.setter

--- src/catalog.py
+++ src/catalog.py
@@ -3,14 +3,14 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
@@ -33,6 +33,21 @@ class Product:
                     return product
 
         return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            if new_price < self.__price:
+                price_drop = input("\nСогласны ли Вы на снижение цены? (y/n)  ")
+                if price_drop.lower() != "y":
+                    return
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
 
 class Category:
@@ -65,15 +80,3 @@ class Category:
         for product in self.__products:
             products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
         return products_str
-
-
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
-
-    print(category1.products)
