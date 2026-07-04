c076a3ff571de126180e71cc79c1eb4593eceda9
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-07-04 06:03:52 +0500

    tested getter, setter, classmethod

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,3 +39,13 @@ def category_tv(product_qled):
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
         [product_qled],
     )
+
+
+@pytest.fixture
+def product_dict():
+    return {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
